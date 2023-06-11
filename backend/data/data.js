98e154629996b480b15f64545dba5a3f8d4c86d0
Yezuss <69847400+Yezuss@users.noreply.github.com>
2023-06-12 03:52:40 +0700

    commit

---
+++ backend/data/data.js
@@ -0,0 +1,109 @@
+const chats = [
+    {
+      isGroupChat: false,
+      users: [
+        {
+          name: "Thanh Nam",
+          email: "thanhnam001@gmail.com",
+        },
+        {
+          name: "Yezuss",
+          email: "tangthanhduc113@gmail.com",
+        },
+      ],
+      _id: "617a077e18c25468bc7c4dd4",
+      chatName: "Thanh Nam",
+    },
+    {
+      isGroupChat: false,
+      users: [
+        {
+          name: "Phuoc Duyen",
+          email: "phuocduyen123@gmail.com",
+        },
+        {
+          name: "Yezuss",
+          email: "tangthanhduc113@gmail.com",
+        },
+      ],
+      _id: "617a077e18c25468b27c4dd4",
+      chatName: "Phuoc Duyen",
+    },
+    {
+      isGroupChat: false,
+      users: [
+        {
+          name: "Tu Phuong",
+          email: "tuphuong002@gmail.com",
+        },
+        {
+          name: "Yezuss",
+          email: "tangthanhduc113@gmail.com",
+        },
+      ],
+      _id: "617a077e18c2d468bc7c4dd4",
+      chatName: "Tu Phuong",
+    },
+    {
+      isGroupChat: true,
+      users: [
+        {
+          name: "Thanh Nam",
+          email: "thanhnam001@gmail.com",
+        },
+        {
+          name: "Yezuss",
+          email: "tangthanhduc113@gmail.com",
+        },
+        {
+          name: "Phuoc Duyen",
+          email: "phuocduyen123@gmail.com",
+        },
+      ],
+      _id: "617a518c4081150716472c78",
+      chatName: "Start Gaming Group",
+      groupAdmin: {
+        name: "Phuoc Duyen",
+        email: "phuocduyen123@gmail.com",
+      },
+    },
+    {
+      isGroupChat: false,
+      users: [
+        {
+          name: "Ryan",
+          email: "ryan007@gmail.com",
+        },
+        {
+          name: "Yezuss",
+          email: "tangthanhduc113@gmail.com",
+        },
+      ],
+      _id: "617a077e18c25468bc7cfdd4",
+      chatName: "Ryan",
+    },
+    {
+      isGroupChat: true,
+      users: [
+        {
+          name: "Thanh Nam",
+          email: "thanhnam001@gmail.com",
+        },
+        {
+          name: "Yezuss",
+          email: "tangthanhduc113@gmail.com",
+        },
+        {
+          name: "Phuoc Duyen",
+          email: "phuocduyen123@gmail.com",
+        },
+      ],
+      _id: "617a518c4081150016472c78",
+      chatName: "Chill Music Group",
+      groupAdmin: {
+        name: "Phuoc Duyen",
+        email: "phuocduyen123@gmail.com",
+      },
+    },
+  ];
+  module.exports={chats}; 
